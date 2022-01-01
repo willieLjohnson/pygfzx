d4ad0e0cd088b4efc758328228b9d90d0a4dd6ca
Willie Johnson <liwa.johnson@gmail.com>
2021-12-31 21:09:10 -0500

    add vector math support

---
+++ vector.py
@@ -0,0 +1,8 @@
+import math
+
+def addVectors(angle1, length1, angle2, length2):
+    x = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    y = math.cos(angle1) * length1 + math.cos(angle2) * length2
+    length = math.hypot(x, y)
+    angle = 0.5 * math.pi - math.atan2(y, x)
+    return (angle, length)
